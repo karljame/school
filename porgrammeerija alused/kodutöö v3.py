1f6b3ef642ed6bb43ad5d72b2606b94dde8252b1
karljame <55434286+karljame@users.noreply.github.com>
2019-11-14 18:36:49 +0200

    Add files via upload

--- porgrammeerija alused/kodutöö v3.py
+++ porgrammeerija alused/kodutöö v3.py
@@ -1,4 +1,3 @@
-list1 = [9, 10, 8, 12, 6, 12, ]
-list1.sort()
-list1 = list(dict.fromkeys(list1))
-print("spordivõistluse tulemused paremuselt teise tulemuselt:", list1[-2])
+n=int(input("Sisend:  "))
+listv3 = [k * k for k in range(0, n)]
+print("Väljund: ", ", ".join( repr(e) for e in listv3 ) )
